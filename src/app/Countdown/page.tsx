ba72a207cbc78edeb5d74d493b7961b3ddacbed4
Soratun <tunzax014@gmail.com>
2024-12-12 17:51:30 +0700

    Add image rotation effect to Countdown component and update target date in Home component

--- src/app/Countdown/page.tsx
+++ src/app/Countdown/page.tsx
@@ -1,6 +1,8 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Fireworks from 'fireworks-js';
+import classNames from 'classnames';
+import Image from 'next/image';
 
 const Countdown = () => {
   const fireworksContainerRef = useRef<HTMLDivElement>(null);
@@ -42,6 +44,18 @@ const Countdown = () => {
     }
   }, []);
 
+  const [imageIndex, setImageIndex] = useState(0);
+    const totalImages = 6;
+
+
+    // Image rotation effect
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
+        }, 5000); // Change every 5 seconds
+        return () => clearInterval(intervalId);
+    }, [totalImages]);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-[#FFD08F] to-[#F26B9C] bg-fixed text-white">
@@ -52,6 +66,25 @@ const Countdown = () => {
         <h1 className="text-7xl font-bold mt-3">
           Happy Birthday Satchan BNK48
         </h1>
+
+        <div className="relative w-[700px] h-[700px] overflow-hidden ">
+                        {[...Array(totalImages)].map((_, index) => (
+                            <Image
+                                key={index}
+                                src={`/img/${index + 1}.png`}
+                                alt={`Satchan BNK48 ${index}`}
+                                layout="fill"
+                                objectFit="contain"
+                                className={classNames(
+                                    "absolute transition-opacity duration-1000 ease-in-out",
+                                    {
+                                        "opacity-100": index === imageIndex,
+                                        "opacity-0": index !== imageIndex,
+                                    }
+                                )}
+                            />
+                        ))}
+            </div>
       </div>
     </>
   );
