45fb921f72e4321b075baf44da65226ceaa2ce4f
Soratun <tunzax014@gmail.com>
2024-12-12 21:12:27 +0700

    Simplify fireworks initialization in Countdown component by removing unused configuration options

--- src/app/Countdown/page.tsx
+++ src/app/Countdown/page.tsx
@@ -10,30 +10,7 @@ const Countdown = () => {
   useEffect(() => {
     if (fireworksContainerRef.current) {
       // เริ่มต้น fireworks เมื่อเข้าหน้านี้
-      const fireworks = new Fireworks(fireworksContainerRef.current, {
-        // maxRockets: 5,         // จำนวนลูกไฟสูงสุด
-        // rocketSpawnInterval: 150, // ช่วงเวลาการสร้างลูกไฟ
-        // numParticles: 100,    // จำนวนอนุภาคของลูกไฟ
-        // explosionMinHeight: 0.2,  // ความสูงขั้นต่ำ
-        rocketsPoint: {
-          min: 1,
-          max: 5
-        },         // จำนวนลูกไฟสูงสุด
-        hue: {
-          min: 10,
-          max: 150
-        }, // ช่วงเวลาการสร้างลูกไฟ
-        delay: {
-          min: 50,
-          max: 100
-        },    // จำนวนอนุภาคของลูกไฟ
-        lineWidth: {
-          explosion: {
-            min: 1,
-            max: 3
-          }
-        },  // ความสูงขั้นต่ำ
-      });
+      const fireworks = new Fireworks(fireworksContainerRef.current);
       
       fireworks.start();  // เริ่มแสดงลูกไฟ
 
